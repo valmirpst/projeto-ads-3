5d072692361f72fe72d78659bb0c9bc0ebab68b4
Valmir Paiva Stachin <valmir@unimake.com.br>
2026-08-19 21:20:58 -0300

    feat: Remove uso de settings não mais utilizado em home.ts

--- frontend/pages/home.ts
+++ frontend/pages/home.ts
@@ -1,4 +1,3 @@
-import { settingsService } from "../services/settings-service.js";
 import { sectionsService } from "../services/sections-service.js";
 import { renderSection } from "../components/sections/SectionRenderer.js";
 
@@ -18,49 +17,8 @@ async function renderSections() {
   }
 }
 
-async function renderSettings() {
-  const { data: settings } = await settingsService.fetchSettings();
-
-  const site_name = settings?.site_name;
-  const site_description = settings?.site_description;
-  const contact_email = settings?.contact_email;
-  const phone = settings?.phone;
-  const instagram = settings?.instagram;
-  const facebook = settings?.facebook;
-  const linkedin = settings?.linkedin;
-
-  const elSiteName = document.getElementById("site-name");
-  const elSiteDescription = document.getElementById("site-description");
-  const elContactEmail = document.getElementById("contact-email");
-  const elContactPhone = document.getElementById("contact-phone");
-
-  if (elSiteName) elSiteName.textContent = site_name || "Sem Nome";
-  if (elSiteDescription) elSiteDescription.textContent = site_description || "";
-  if (elContactEmail) elContactEmail.textContent = contact_email || "-";
-  if (elContactPhone) elContactPhone.textContent = phone || "-";
-
-  const elInstagram = document.getElementById("link-instagram") as HTMLAnchorElement;
-  if (instagram && instagram) {
-    elInstagram.href = instagram;
-    elInstagram.style.display = "inline";
-  }
-
-  const elFacebook = document.getElementById("link-facebook") as HTMLAnchorElement;
-  if (facebook && facebook) {
-    elFacebook.href = facebook;
-    elFacebook.style.display = "inline";
-  }
-
-  const elLinkedin = document.getElementById("link-linkedin") as HTMLAnchorElement;
-  if (linkedin && linkedin) {
-    elLinkedin.href = linkedin;
-    elLinkedin.style.display = "inline";
-  }
-}
-
 async function main() {
-  // Execute calls in parallel
-  await Promise.all([renderSections(), renderSettings()]);
+  await renderSections();
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
